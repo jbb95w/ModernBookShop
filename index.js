91e7f681d27394f4caa110327071b6853defeb92
jabir <jabiralinoor2019@gmail.com>
2025-06-26 12:49:34 +0300

    Get elements from HTML using getElementById

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Event Listener #1: Run code after page fully loads
 window.addEventListener("DOMContentLoaded", start);
+// Get HTML elements by their IDs
+const searchButton = document.getElementById("searchBtn");
+const searchInput = document.getElementById("searchInput");
+const resultsArea = document.getElementById("results");
